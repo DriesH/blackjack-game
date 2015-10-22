78ffa0ed78c1c59bc2eb9baa246c98d951bf1544
DriesH <dries.heyninck@gmail.com>
2015-10-22 15:38:20 +0200

    added basic classes for bet view

---
+++ blackjack-game/blackjack-game/PlayerBetView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace blackjack_game
+{
+    public partial class PlayerBetView : UserControl
+    {
+        PlayerBetController _playerBetController;
+
+        public PlayerBetView(PlayerBetController _controller)
+        {
+            _playerBetController = _controller;
+            InitializeComponent();
+        }
+
+        private void PlayerBetView_Load(object sender, EventArgs e)
+        {
+            Controls.Add(_playerBetController.getView());
+        }
+    }
+}
